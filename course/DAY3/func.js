d9bab184ff2917b6353fb4a545eb79522ed14d13
j1mmyson <bwson98@gmail.com>
2021-01-13 12:41:04 +0900

    DAY3: 함수

---
+++ course/DAY3/func.js
@@ -0,0 +1,15 @@
+
+
+const calculator = {
+    plus: function(a, b){
+        return a+b;
+    },
+    minus: function(a, b){
+        return a-b;
+    },
+}
+
+const plus = calculator.plus(5, 10);
+console.log(plus);
+
+console.log(calculator);
